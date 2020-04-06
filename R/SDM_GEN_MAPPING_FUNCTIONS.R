1c42ca13d0cb0494ec0adbcc3872ddf7239790c1
Hugh Burley <hugh.burley@gmail.com>
2020-04-06 18:31:42 +1000

    Update SDM_GEN_MAPPING_FUNCTIONS.R
    - Fixed the SDM problem with reading the shapefiles as RDS objects ::
    
      areal_unit <- readRDS(unit_shp)
      aus_poly   <- readRDS(aus_shp)
      world_poly <- readRDS(world_shp)

--- R/SDM_GEN_MAPPING_FUNCTIONS.R
+++ R/SDM_GEN_MAPPING_FUNCTIONS.R
@@ -629,20 +629,6 @@ polygonizer_windows <- function(x, outshape=NULL, pypath=NULL, readpoly=TRUE,
 }
 
 
-# unit_shp      = './data/SUA_albers.rds'                     ## Spatial unit of analysis - E.G. SUAs in Australian Albers
-# unit_vec      = areal_unit_vec                   ## Vector of rasterized unit cells
-# sort_var      = "SUA_NAME16"
-# code_var      = "SUA_CODE16"
-# world_shp     = './data/LAND_albers.rds'         ## Polygon for AUS maps
-# aus_shp       = './data/AUS_albers.rds'          ## Polygon for World maps
-#
-# DIR_list      = sdm.results.dir                  ## List of directories with rasters
-# species_list  = map_spp                          ## List of species' directories
-# number_gcms   = 6
-# maxent_path   = 'output/maxent/back_sel_models/' ## Directory of maxent results
-# thresholds    = percent.10.log                   ## List of maxent thresholds
-# time_slice    = 30                               ## Time period eg 2030
-# write_rasters = TRUE
 
 
 
@@ -658,10 +644,6 @@ area_cell_count = function(unit_shp, aus_shp, world_shp, sort_var,
   areal_unit <- readRDS(unit_shp)
   aus_poly   <- readRDS(aus_shp)
   world_poly <- readRDS(world_shp)
-
-  ## Change this so it's not hard wired to a particular field name
-  ## areal_unit      = areal_unit[order(areal_unit$SUA_NAME16),]
-  #areal_unit = areal_unit[order(areal_unit[[sort_var]]),]
 
   ## Loop over each directory
   ## DIR = DIR_list[1]
